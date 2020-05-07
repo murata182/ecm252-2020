973fe171988c79ac621ed7d9b186f9b2870c3767
Rafael Murata <rafael_murata@hotmail.com>
2020-05-07 15:48:11 -0300

    Aula 06 - Exercicio

--- Aula_06/jogo/app.js
+++ Aula_06/jogo/app.js
@@ -3,6 +3,8 @@ var pontuacaoRodada = 0;
 var jogadorAtual = 0;
 
 var botaoLancarDado = document.querySelector("#botao-lancar-dado");
+var botaoPassarVez = document.querySelector("#botao-passar-vez");
+var botaoNovoJogo = document.querySelector("#botao-novo-jogo");
 var pontuacaoRodada0 = document.querySelector("#pontuacao-rodada-0");
 var pontuacaoRodada1 = document.querySelector("#pontuacao-rodada-1");
 var pontuacaoGlobal0 = document.querySelector("#pontuacao-global-0");
@@ -10,6 +12,26 @@ var pontuacaoGlobal1 = document.querySelector("#pontuacao-global-1");
 
 var pontuacoesRodadas = [pontuacaoRodada0, pontuacaoRodada1];
 var pontuacoesGlobais = [pontuacaoGlobal0, pontuacaoGlobal1];
+
+function novoJogo() {
+    pontuacao = [0, 0];
+    pontuacoesRodadas[0].textContent = 0;
+    pontuacoesRodadas[1].textContent = 0;
+    pontuacoesGlobais[0].textContent = 0;
+    pontuacoesGlobais[1].textContent = 0;
+
+    var novoJogador = Math.round(Math.random());
+    jogadorAtual = novoJogador;
+}
+
+function mostrarVencedor() {
+    var vencedor = ""
+    $("#myModal").on("show.bs.modal", function (event) {
+        vencedor = "O jogador " + (jogadorAtual + 1) + " venceu!";
+        $(this).find(".modal-title").text(vencedor);
+    })
+    $("#myModal").modal('show');
+}
 
 botaoLancarDado.addEventListener("click", function () {
     var dado = Math.floor(Math.random() * 6) + 1;
@@ -23,6 +45,17 @@ botaoLancarDado.addEventListener("click", function () {
         pontuacao[jogadorAtual] += dado;
         pontuacoesGlobais[jogadorAtual].textContent = pontuacao[jogadorAtual];
         pontuacoesRodadas[jogadorAtual].textContent = dado;
+        if (pontuacao[jogadorAtual] >= 100) {
+            mostrarVencedor();
+            novoJogo();
+        }
     }
 });
 
+botaoPassarVez.addEventListener("click", function () {
+    jogadorAtual = 1 - jogadorAtual;
+});
+
+botaoNovoJogo.addEventListener("click", function () {
+    novoJogo();
+});
