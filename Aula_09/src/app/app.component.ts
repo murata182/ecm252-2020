5a7ad89c72649348b15dd837f61d12cf559c8afc
Rafael Murata <rafael_murata@hotmail.com>
2020-05-23 12:24:43 -0300

    Aula 09

---
+++ Aula_09/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  pessoas = [
+    { nome: "José", idade: 18 },
+    { nome: "Maria", idade: 22 }
+  ]
+  onAdicionarPessoa(pessoa) {
+    this.pessoas = [pessoa, ...this.pessoas];
+  }
+
+  textoBotao = "Esconder";
+  esconder = false;
+
+  alterarExibicao() {
+    this.textoBotao = this.esconder ? "Exibir" : "Esconder";
+    this.esconder = !this.esconder;
+  }
+
+
+}
