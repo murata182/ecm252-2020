1765fbbf2c6a335627e1c951c3aa9f915e205982
Rafael Murata <rafael_murata@hotmail.com>
2020-05-30 12:58:36 -0300

    Aula 10

---
+++ Aula_10/maua-forms/src/app/formulario/formulario.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Pessoa } from '../model/pessoa';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent {
+
+  pessoa: Pessoa = new Pessoa();
+
+  profissoes = ['Engenheiro', 'Professor', 'Químico', 'Zoólogo', 'Outros'];
+
+  salvar(pessoaForm): void {
+    /*this.pessoa.nome = pessoaForm.value.nome;
+    this.pessoa.fone = pessoaForm.value.fone;
+    this.pessoa.email = pessoaForm.value.email;
+    this.pessoa.profissao = pessoaForm.value.profissao;*/
+
+    console.log(this.pessoa);
+    console.log(pessoaForm);
+  }
+}
